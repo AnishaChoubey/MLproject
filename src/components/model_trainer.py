1a1f5e94071cd69f0025c425b12cc238852866db
Anisha Choubey <anishachoubey396@gmail.com>
2025-08-27 22:16:31 +0530

    Hyperparamter Training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -42,21 +42,67 @@ class ModelTrainer:
                 'Decision Tree':DecisionTreeRegressor(),
                 'Gradient Boosting':GradientBoostingRegressor(),
                 'Linear Regression':LinearRegression(),
-                'K-Neighbors Classifier':KNeighborsRegressor(),
-                'XGBClassifier':XGBRegressor(),
-                'CatBoosting Classifier': CatBoostRegressor(verbose=0),
-                'AdaBoost Classifier':AdaBoostRegressor()
+                'KNeighbors Regressor':KNeighborsRegressor(),
+                'XGBRegressor':XGBRegressor(),
+                'CatBoosting Regressor': CatBoostRegressor(verbose=0),
+                'AdaBoost Regressor':AdaBoostRegressor()
             }
 
-            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
 
-            ## To get the best model score from dict
-            best_model_score=max(sorted(model_report.values()))
+                },
+                "Random Forest":{
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "KNeighbors Regressor":{
+                    'n_neighbors':[5,7,9,11],
+                    'weights':['uniform','distance'],
+                    'metric':['minkowski','euclidean','manhattan']
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[30,50,100]
 
-            ## To get the best model name from dict
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth':[6,8,10],
+                    'learning_rate':[0.01,0.05,0.1],
+                    'iterations':[30,50,100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
+            }
 
-            best_model_name=list(model_report.keys())[
-                list(model_report.values()).index(best_model_score)]
+            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
+
+            # ## To get the best model score from dict
+            # best_model_score=max(sorted(model_report.values()))
+
+            # ## To get the best model name from dict
+
+            # best_model_name=list(model_report.keys())[
+            #     list(model_report.values()).index(best_model_score)]
+            # Extract only the scores for comparison
+            best_model_score = max([score[0] for score in model_report.values()])
+
+            # Get the best model name
+            best_model_name = list(model_report.keys())[
+            [score[0] for score in model_report.values()].index(best_model_score)]
+
             best_model=models[best_model_name]
 
             if best_model_score<0.6:
